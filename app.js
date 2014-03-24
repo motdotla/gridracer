d8ead0794c4271984ca10b4a959ea0388b2bfbfd
scottmotte <scott@scottmotte.com>
2014-03-23 21:38:29 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+var dotenv      = require('dotenv');
+dotenv.load();
+
+var gpio        = require("gpio");
+
+// Constants
+var e               = module.exports;
+e.ENV               = process.env.NODE_ENV || 'development';
+var FORWARD_PIN     = 18;
+var LEFT_PIN        = 23;
+var DEFAULT_TIMEOUT = 700;
+var COMMAND_TIMEOUT = 300;
+var forward;
+var left;
+
+var port        = parseInt(process.env.PORT) || 3000;
+var Hapi        = require('hapi');
+server          = new Hapi.Server(+port, '0.0.0.0', { cors: true });
+
+var directions = {
+  forward: {
+    handler: function (request) {
+      var payload   = request.payload;
+      console.log(payload);
+
+      forward = gpio.export(FORWARD_PIN, {
+        direction: 'out',
+        ready: function() {
+          setTimeout(function() {
+            forward.set();
+            setTimeout(function() {
+              forward.removeAllListeners('change');
+              forward.reset();
+              forward.unexport();
+              forward.unexport(function() {
+                request.reply({success: true});
+              });
+            }, DEFAULT_TIMEOUT);
+          }, COMMAND_TIMEOUT);
+        }
+      });
+    }
+  }
+};
+
+server.route({
+  method  : 'GET',
+  path    : '/forward',
+  config  : directions.forward
+});
+
+server.start(function() {
+  console.log('Server started at: ' + server.info.uri);
+});
